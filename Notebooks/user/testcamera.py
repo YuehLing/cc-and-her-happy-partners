c06325514c252eef4c5c9ebbfaf77f212a9dc3ef
YuehLing <56074871+YuehLing@users.noreply.github.com>
2021-10-27 09:15:23 +0800

    Add files via upload

---
+++ Notebooks/user/testcamera.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# MIT License
+
+# Copyright (c) 2017-18 Jetsonhacks
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import sys
+import argparse
+import cv2
+import numpy as np
+
+def parse_cli_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video_device", dest="video_device",
+                        help="Video device # of USB webcam (/dev/video?) [0]",
+                        default=0, type=int)
+    arguments = parser.parse_args()
+    return arguments
+
+# On versions of L4T previous to L4T 28.1, flip-method=2
+# Use the Jetson onboard camera
+def open_onboard_camera():
+    return cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+
+# Open an external usb camera /dev/videoX
+def open_camera_device(device_number):
+    return cv2.VideoCapture(device_number)
+   
+
+def read_cam(video_capture):
+    if video_capture.isOpened():
+        windowName = "CannyDemo"
+        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(windowName,1280,720)
+        cv2.moveWindow(windowName,0,0)
+        cv2.setWindowTitle(windowName,"Canny Edge Detection")
+        showHelp = True
+        font = cv2.FONT_HERSHEY_PLAIN
+        helpText="'Esc' to Quit, '1' for Camera Feed, '2' for Canny Detection, '3' for All Stages. '4' to hide help"
+        showFullScreen = False
+        while True:
+            if cv2.getWindowProperty(windowName, 0) < 0: # Check to see if the user closed the window
+                # This will fail if the user closed the window; Nasties get printed to the console
+                break;
+            ret_val, frame = video_capture.read();
+            
+            img_red = cv2.inRange(frame,(0,0,100),(100,100,255))
+            image = cv2.bitwise_and(frame,frame,mask = img_red)
+            
+
+            displayBuf = image
+
+            if showHelp == True:
+                cv2.putText(displayBuf, helpText, (11,20), font, 1.0, (32,32,32), 4, cv2.LINE_AA)
+                cv2.putText(displayBuf, helpText, (10,20), font, 1.0, (240,240,240), 1, cv2.LINE_AA)
+            cv2.imshow(windowName,displayBuf)
+            key=cv2.waitKey(10)
+            if key == 27: # Check for ESC key
+                cv2.destroyAllWindows()
+                break ;
+            elif key==52: # 4 key, toggle help
+                showHelp = not showHelp
+            elif key==74: # Toggle fullscreen; This is the F3 key on this particular keyboard
+                # Toggle full screen mode
+                if showFullScreen == False : 
+                    cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                else:
+                    cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL) 
+                showFullScreen = not showFullScreen
+              
+    else:
+     print ("camera open failed")
+
+
+
+if __name__ == '__main__':
+    arguments = parse_cli_args()
+    print("Called with args:")
+    print(arguments)
+    print("OpenCV version: {}".format(cv2.__version__))
+    print("Device Number:",arguments.video_device)
+    if arguments.video_device==0:
+      video_capture=open_onboard_camera()
+    else:
+      video_capture=open_camera_device(arguments.video_device)
+    read_cam(video_capture)
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+#https://github.com/JetsonHacksNano/buildOpenCV.git
